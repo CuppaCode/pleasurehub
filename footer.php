9bb174f8e197a26f605304546296fade518f90bc
General Orange <erik.17021995@gmail.com>
2022-10-27 21:55:36 +0200

    Added footer and finished category section

---
+++ footer.php
@@ -0,0 +1,83 @@
+<?php
+/**
+ * The template for displaying the footer
+ *
+ * Contains the closing of the #content div and all content after
+ *
+ * @package Understrap
+ */
+
+// Exit if accessed directly.
+defined( 'ABSPATH' ) || exit;
+
+$container = get_theme_mod( 'understrap_container_type' );
+?>
+
+<?php get_template_part( 'sidebar-templates/sidebar', 'footerfull' ); ?>
+
+<div class="wrapper" id="wrapper-footer">
+
+	<div class="<?php echo esc_attr( $container ); ?>">
+
+		<div class="row">
+
+			<div class="col-md-12">
+
+			<footer class="py-5">
+				<div class="row">
+                    <div class="col-2">
+						<?php if ( is_active_sidebar( 'footer-1' ) ) : ?>
+                            <?php dynamic_sidebar( 'footer-1' ); ?>
+                        <?php endif; ?>
+					</div>
+
+
+					<div class="col-2">
+						<?php if ( is_active_sidebar( 'footer-2' ) ) : ?>
+                            <?php dynamic_sidebar( 'footer-2' ); ?>
+                        <?php endif; ?>
+					</div>
+
+
+					<div class="col-2">
+						<?php if ( is_active_sidebar( 'footer-3' ) ) : ?>
+                            <?php dynamic_sidebar( 'footer-3' ); ?>
+                        <?php endif; ?>
+					</div>
+
+					<div class="col-4 offset-1">
+					<div class="col">
+						<a href="/" class="d-flex align-items-center mb-3 link-dark text-decoration-none">
+                            <?php the_custom_logo(); ?>
+						</a>
+						<p class="text-muted">© 2021</p>
+						</div>
+					</div>
+				</div>
+
+				<div class="d-flex justify-content-between py-4 my-4 border-top">
+				<p>© 2021 Company, Inc. All rights reserved.</p>
+				<ul class="list-unstyled d-flex">
+					<li class="ms-3"><a class="link-dark" href="#"><svg class="bi" width="24" height="24"><use xlink:href="#twitter"></use></svg></a></li>
+					<li class="ms-3"><a class="link-dark" href="#"><svg class="bi" width="24" height="24"><use xlink:href="#instagram"></use></svg></a></li>
+					<li class="ms-3"><a class="link-dark" href="#"><svg class="bi" width="24" height="24"><use xlink:href="#facebook"></use></svg></a></li>
+				</ul>
+				</div>
+			</footer>
+
+			</div><!--col end -->
+
+		</div><!-- row end -->
+
+	</div><!-- container end -->
+
+</div><!-- wrapper end -->
+
+</div><!-- #page we need this extra closing tag here -->
+
+<?php wp_footer(); ?>
+
+</body>
+
+</html>
+
--- template-parts/categories.php
+++ template-parts/categories.php
@@ -1,12 +1,48 @@
+<?php
+
+$args = array(
+    'taxonomy'   => "product_cat",
+    'number'     => $number,
+    'orderby'    => $orderby,
+    'order'      => $order,
+    'hide_empty' => $hide_empty,
+    'include'    => $ids
+);
+$product_categories = get_terms($args);
+
+?>
+
 <section class="categories py-3">
     
     <h2 class="text-center pt-5">Categorieen</h2>
     <p class="text-center">Een greep uit onze categorieen</p>
     
-    <div class="pt-5">
-        <div class="categories__col col-md-4 col-md-12">
-            
-            <img src="" alt="" class="" loading="lazy">
-        </div>
+    <div class="row row-cols-1 row-cols-md-3 g-4">
+
+        <?php foreach( $product_categories as $category ) : ?>
+            <div class="col">
+                <div class="card mb-3" style="max-width: 540px;">
+                    <div class="row g-0">
+                        <div class="col-md-4">
+                            <img src="<?= get_stylesheet_directory_uri(). '/img/slider/zonnepaneel-1.jpg'; ?>" class="img-fluid rounded-start" alt="...">
+                        </div>
+                        <div class="col-md-8">
+                        <div class="card-body">
+                            <h5 class="card-title"><?= $category->name; ?></h5>
+
+                            <?php if (!empty( $category->description ) ) : ?>
+
+                                <p class="card-text"><?= $category->description ?></p>
+
+                            <?php endif; ?>
+                            
+                            <a href="<?= get_term_link( $category ); ?>" class="btn btn-outline-primary btn-block">Naar categorie</a>
+                        </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        <?php endforeach; ?>
+
     </div>
 </section>
