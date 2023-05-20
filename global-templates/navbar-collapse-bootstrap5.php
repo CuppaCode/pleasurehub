42b58e8179661aac9fbf27fb9b60d3ad9f2bd947
General Orange <erik.17021995@gmail.com>
2023-05-20 03:19:19 +0200

    Made superrior menu array

--- global-templates/navbar-collapse-bootstrap5.php
+++ global-templates/navbar-collapse-bootstrap5.php
@@ -22,68 +22,113 @@ $container = get_theme_mod( 'understrap_container_type' );
         <div class="nav-menus-wrapper">
 		<?php 
 		
-		echo '<pre>';
-		var_dump( wp_get_nav_menu_items('menu-1',array ( 'post_type' => 'nav_menu_item')) );
-		echo '</pre>';
 
 		$menu_items = wp_get_nav_menu_items('menu-1',array ( 'post_type' => 'nav_menu_item'));
 		$menu_list = '';
 		$bool = false;
-
-		$menu_list .= '<ul class="nav-menu">' ."\n";
+		
+		// Determine first level menu items
 		foreach( $menu_items as $menu_item ) {
 			if( $menu_item->menu_item_parent == 0 ) {
 				
-				$top_level_menu[] = $menu_item;
+				$top_level_menu[$menu_item->ID] = $menu_item;
+				$top_level_menu[$menu_item->ID]->children = [];
 			} else {
 				$lower_level_menu[] = $menu_item;
 			}
 		}
 
-		foreach( $lower_level_menu as $lower_level_menu_item ) {
-			if ( $lower_level_menu_item->classes[0] == 'submenu-parent' ) {
-				$header_submenu[] = $lower_level_menu_item;
-			} else {
+		// Determine second level menu items
+		if(count($lower_level_menu) > 0){
+
+			foreach( $lower_level_menu as $lower_id => $lower_level_menu_item ) {
 				
+
+				foreach($top_level_menu as $id => $top_level_menu_item) {
+					
+					if($lower_level_menu_item->menu_item_parent == $id) {
+
+						$top_level_menu[$id]->children[$lower_level_menu_item->ID] = $lower_level_menu_item;
+						$top_level_menu[$id]->children[$lower_level_menu_item->ID]->children = [];
+						unset($lower_level_menu[$lower_id]);
+
+					}
+					
+				}
+
 			}
 
 		}
 
+		// Determine third level menu items
+		if(count($lower_level_menu) > 0){
 
-			// 	$parent = $menu_item->ID;
+			foreach( $lower_level_menu as $lower_id => $lower_level_menu_item ) {
 				
-			// 	$menu_array = array();
-			// 	foreach( $menu_items as $submenu ) {
-			// 		if( $submenu->menu_item_parent == $parent ) {
-			// 			$bool = true;
-			// 			$menu_array[] = '<li class="'. $submenu->classes[0] .'"><a href="' . $submenu->url . '">' . $submenu->title . '</a></li>' ."\n";
-			// 		}
-			// 	}
-			// 	if( $bool == true && count( $menu_array ) > 0 ) {
-					
 
-			// 		$menu_list .= '<li>' ."\n";
-			// 		$menu_list .= '<a href="#">' . $menu_item->title ."\n";
-			// 		$menu_list .= '<div class="megamenu-panel"><div class="megamenu-lists">';
-			// 		if ( $menu_item->classes[0] == 'submenu-parent' ) {
-			// 			$menu_list .= '<ul class="megamenu-list list-col-4">' ."\n";
-			// 		}
+				foreach($top_level_menu as $id => $top_level_menu_item) {
 					
-			// 		$menu_list .= implode( "\n", $menu_array );
-			// 		$menu_list .= '</ul>' ."\n";
-			// 		$menu_list .= '</div></div>';
+					if(count($top_level_menu_item->children) > 0) {
+
+						foreach($top_level_menu_item->children as $second_level_id => $second_level_item){
+
+							if($lower_level_menu_item->menu_item_parent == $second_level_id) {
+
+								$second_level_item->children[$lower_id] = $lower_level_menu_item;
+								unset($lower_level_menu[$lower_id]);
+
+							}
+
+						}
+
+					}
 					
-			// 	} else {
-					
-			// 		$menu_list .= '<li>' ."\n";
-			// 		$menu_list .= '<a href="' . $menu_item->url . '">' . $menu_item->title . '</a>' ."\n";
-			// 	}
-				
-			// }
+				}
+
+			}
+
+		}
+
+		// Dump the menu, if need be...
+		// echo '<pre>';
+		// var_dump($top_level_menu);
+		// echo '</pre>';
+
+
+
+		$menu_list .= '<ul class="nav-menu">' ."\n";
+		//$parent = $menu_item->ID;
+		
+
+		foreach( $top_level_menu as $menu_item ) {
+
+			if( count($menu_item->children) > 0) {
+
+				//$menu_list .= '<li class="'. $submenu->classes[0] .'"><a href="' . $submenu->url . '">' . $submenu->title . '</a></li>' ."\n";
+
+			} else {
+
+				$menu_list .= '<li>' ."\n";
+				$menu_list .= '<a href="' . $menu_item->url . '">' . $menu_item->title . '</a>' ."\n";
+				$menu_list .= '</li>' ."\n";
+
+			}
+		}
+		if( $bool == true && count( $menu_array ) > 0 ) {
 			
-			// end <li>
-		// 	$menu_list .= '</li>' ."\n";
-        // }
+
+			$menu_list .= '<li>' ."\n";
+			$menu_list .= '<a href="#">' . $menu_item->title ."\n";
+			$menu_list .= '<div class="megamenu-panel"><div class="megamenu-lists">';
+			if ( $menu_item->classes[0] == 'submenu-parent' ) {
+				$menu_list .= '<ul class="megamenu-list list-col-4">' ."\n";
+			}
+			
+			$menu_list .= implode( "\n", $menu_array );
+			$menu_list .= '</ul>' ."\n";
+			$menu_list .= '</div></div>';
+			
+		}
          
         $menu_list .= '</ul>' ."\n";
 
